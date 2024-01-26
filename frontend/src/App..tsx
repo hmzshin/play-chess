65d467a2e4e249e3ec7a41831daab680962959f7
hmzshin <hamzasahin@outlook.com>
2024-01-26 21:29:26 +0300

    refactor: change directory

--- frontend/src/App..tsx
+++ frontend/src/App..tsx
@@ -1,9 +1,10 @@
 import "./App.css";
-import React from "react";
+import React, { useEffect } from "react";
 import Header from "./components/Header.tsx";
 import ChessTable from "./components/ChessTable.tsx";
 
 function App() {
+
   return (
     <>
       <Header />
