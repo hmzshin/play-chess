acd14c6f5f267f2d3bcb6de7ca3caadfb0120463
hmzshin <hamzasahin@outlook.com>
2024-01-26 21:30:23 +0300

    feature: import socket io client for frontend app

--- frontend/src/App..tsx
+++ frontend/src/App..tsx
@@ -2,8 +2,18 @@ import "./App.css";
 import React, { useEffect } from "react";
 import Header from "./components/Header.tsx";
 import ChessTable from "./components/ChessTable.tsx";
+import { io } from "socket.io-client";
+import { toast } from "react-toastify";
 
 function App() {
+  useEffect(() => {
+    const socket = io("http://localhost:3000");
+    socket.on("connect", () => {
+      toast.success(`You connected with id ${socket.id}`);
+      console.log(socket.id);
+    });
+    socket.emit("customEvent", 10, "Hello");
+  }, []);
 
   return (
     <>
