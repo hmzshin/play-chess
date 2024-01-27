ba484588443bdecce8d0dcb4da26ab72f3484509
hmzshin <hamzasahin@outlook.com>
2024-01-27 13:37:07 +0300

    feature: Create a server with socket.io to instantly communicate between two players

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,33 @@
 const io = require("socket.io")(3000, {
   cors: {
-    origin: ["http://localhost:5173/play-chess"],
-    methods: "GET,PUT,POST,DELETE",
+    origin: "http://localhost:5173/play-chess",
+    methods: ["GET", "PUT", "POST", "DELETE"],
   },
 });
 
 io.on("connection", (socket) => {
-  console.log(socket.id);
-  socket.on("customEvent", (number, string) => console.log(number, string));
+  console.log(`User: ${socket.id} is connected to server.`);
+  socket.on("sendMove", (data) => {
+    if (data.room !== "" && data.username !== "") {
+      socket.to(data.room).emit("receiveMove", data);
+      console.log(data);
+    }
+  });
+
+  socket.on("joinRoom", (room, callback) => {
+    const roomClients = io.sockets.adapter.rooms.get(room);
+    if (roomClients && roomClients.size > 0) {
+      socket.join(room);
+      callback("black");
+      console.log(`User with id: ${socket.id} is connected to room: ${room}`);
+    } else {
+      socket.join(room);
+      callback("white");
+      console.log(`User with id: ${socket.id} is connected to room: ${room}`);
+    }
+  });
+
+  socket.on("disconnect", () =>
+    console.log(`User: ${socket.id} is disconnected.`)
+  );
 });
