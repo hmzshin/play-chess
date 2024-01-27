f64201039d6becccbf3379bbbd4d61a6f4950067
hmzshin <hamzasahin@outlook.com>
2024-01-27 23:13:35 +0300

    feature: Create GamePage component to display chess table after successfull login

---
+++ frontend/src/pages/GamePage.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "../components/Header";
+import ChessTable from "../components/ChessTable";
+
+const GamePage = () => {
+  return (
+    <>
+      <Header />
+      <ChessTable />
+    </>
+  );
+};
+
+export default GamePage;
