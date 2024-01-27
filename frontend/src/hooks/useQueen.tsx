9f7f1c7319d98d414298dc3955d6c2fac1ebb75b
hmzshin <hamzasahin@outlook.com>
2024-01-27 19:40:25 +0300

    fix: Fix queen moves

--- frontend/src/hooks/useQueen.tsx
+++ frontend/src/hooks/useQueen.tsx
@@ -1,96 +1,162 @@
 import React, { useContext, useEffect, useState } from "react";
+import { chessboard } from "../data";
 import { PossibleMovesContextObject } from "../context/PossibleMovesContext";
 import { TableContextObject } from "../context/TableContext";
 
-type PieceType = "Pawn" | string;
+type PieceType = "Queen" | string;
 type SquareType = "empty" | string;
 
-interface PawnState {
+interface QueenState {
   id: number;
   piece: PieceType;
 }
 
-const usePawn = (initialValue: PawnState) => {
-  const [state, setState] = useState<PawnState>(initialValue);
-  const { dispatchPossibleMoves } = useContext(PossibleMovesContextObject);
-  const { table } = useContext(TableContextObject);
+const useQueen = (initalValue: any) => {
+  const [state, setState] = useState(initalValue);
+  const { dispatchPossibleMoves }: any = useContext(PossibleMovesContextObject);
+  const { table }: any = useContext(TableContextObject);
 
-  const findEmptySquares = (array: [number, SquareType][]): number[] => {
-    const newArray: number[] = [];
+  const findEmptySquares = (array: any) => {
+    const newArray: string[] = [];
+
     for (let i = 0; i < array.length; i++) {
-      if (array[i][1] === "empty") {
+      if (array[i][1] == "empty") {
         newArray.push(array[i][0]);
+      } else {
+        if (array[i][1].includes("black") && state.piece.includes("white")) {
+          console.log("önümde siyah taş var", state.piece);
+          newArray.push(array[i][0]);
+          return newArray;
+        } else if (
+          array[i][1].includes("white") &&
+          state.piece.includes("black")
+        ) {
+          console.log("önümde beyaz taş var", state.piece);
+          newArray.push(array[i][0]);
+          return newArray;
+        }
+        return newArray;
       }
     }
     return newArray;
   };
 
+  const coordinateToIndex = (array: number[]) => {
+    return array.map((coordinate: any) => {
+      for (const key in chessboard) {
+        if (
+          coordinate[0] == chessboard[key][0] &&
+          coordinate[1] == chessboard[key][1]
+        )
+          return Number(key);
+      }
+    });
+  };
   useEffect(() => {
-    if (state.piece.includes("Pawn")) {
-      console.log(`${table[state.id]} is selected`);
-      let possibleMove: number[] = [];
-      const possibleTakes: number[] = [];
-      const copy = { ...state };
-      if (state.piece.includes("white")) {
-        if ([49, 50, 51, 52, 53, 54, 55, 56].includes(state.id)) {
-          possibleMove.push(copy.id - 8);
-          possibleMove.push(copy.id - 16);
-        } else {
-          possibleMove.push(copy.id - 8);
-        }
-        if ([56, 48, 40, 32, 24, 16].includes(copy.id)) {
-          possibleTakes.push(copy.id - 9);
-        } else if ([49, 41, 33, 25, 17, 9].includes(copy.id)) {
-          possibleTakes.push(copy.id - 7);
-        } else {
-          possibleTakes.push(copy.id - 9);
-          possibleTakes.push(copy.id - 7);
-        }
-        var takes = possibleTakes.filter((square) =>
-          table[square].includes("black")
-        );
-      } else {
-        if ([9, 10, 11, 12, 13, 14, 15, 16].includes(state.id)) {
-          possibleMove.push(copy.id + 8);
-          possibleMove.push(copy.id + 16);
-        } else {
-          possibleMove.push(copy.id + 8);
-        }
+    if (state.piece.includes("Queen")) {
+      console.log(`${table[state.id]} seçildi`);
 
-        if ([56, 48, 40, 32, 24, 16].includes(copy.id)) {
-          possibleTakes.push(copy.id + 7);
-        } else if ([49, 41, 33, 25, 17, 9].includes(copy.id)) {
-          possibleTakes.push(copy.id + 9);
-        } else {
-          possibleTakes.push(copy.id + 9);
-          possibleTakes.push(copy.id + 7);
-        }
+      let x = chessboard[state.id][0];
+      let y = chessboard[state.id][1];
+      const south: any[] = [];
+      const west: any[] = [];
+      const north: any[] = [];
+      const east: any[] = [];
+      const southEast: any[] = [];
+      const southWest: any[] = [];
+      const northEast: any[] = [];
+      const northWest: any[] = [];
+      let x1 = x;
+      let y1 = y;
+      let x2 = x;
+      let y2 = y;
+      let x3 = x;
+      let y3 = y;
+      let x4 = x;
+      let y4 = y;
+      let x5 = x;
+      let y5 = y;
+      let x6 = x;
+      let y6 = y;
+      let x7 = x;
+      let y7 = y;
+      let x9 = x;
+      let y9 = y;
 
-        var takes = possibleTakes.filter((square) =>
-          table[square].includes("white")
-        );
+      while (x1 < 8 && y1 < 8) {
+        x1 = x1 + 1;
+        y1 = y1 + 1;
+        southEast.push([x1, y1]);
       }
 
-      const possibleMoves = findEmptySquares(
-        possibleMove.map((square) =>
-          table[square] === "empty"
-            ? [square, "empty"]
-            : [square, table[square]]
+      while (x2 < 8 && y2 > 1) {
+        x2 = x2 + 1;
+        y2 = y2 - 1;
+        southWest.push([x2, y2]);
+      }
+      while (x3 > 1 && y3 < 8) {
+        x3 = x3 - 1;
+        y3 = y3 + 1;
+        northEast.push([x3, y3]);
+      }
+      while (x4 > 1 && y4 > 1) {
+        x4 = x4 - 1;
+        y4 = y4 - 1;
+        northWest.push([x4, y4]);
+      }
+
+      while (x5 < 8) {
+        x5 = x5 + 1;
+        south.push([x5, y5]);
+      }
+
+      while (x6 > 1) {
+        x6 = x6 - 1;
+        north.push([x6, y6]);
+      }
+      while (y7 < 8) {
+        y7 = y7 + 1;
+        east.push([x7, y7]);
+      }
+      while (y9 > 1) {
+        y9 = y9 - 1;
+        west.push([x9, y9]);
+      }
+
+      const combinedDirections = [
+        north,
+        south,
+        east,
+        west,
+        northEast,
+        northWest,
+        southEast,
+        southWest,
+      ];
+
+      const possibleMoves = combinedDirections
+        .map((direction) =>
+          findEmptySquares(
+            coordinateToIndex(direction).map((square: any) =>
+              table[square] == "empty"
+                ? [square, "empty"]
+                : [square, table[square]]
+            )
+          )
         )
-      );
-      const final = [...possibleMoves, ...takes];
+        .reduce((combinedArray, array) => [...combinedArray, ...array]);
+
       dispatchPossibleMoves({
         type: "SET_POSSIBLE_MOVES",
-        payload: final,
+        payload: possibleMoves,
       });
     }
-  }, [state, dispatchPossibleMoves]);
+  }, [state]);
 
-  function changeHandler(val: PawnState) {
+  function changeHandler(val: object) {
     setState(val);
   }
 
-  return [changeHandler] as const;
+  return [changeHandler];
 };
-
-export default usePawn;
+export default useQueen;
